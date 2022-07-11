f320fd599ad14ed3205612e36c3dd337475d5ebe
geisondossantos <geison_bf@hotmail.com>
2022-07-11 17:11:26 -0300

    Open/Closed Principle

---
+++ src/ocp/classes/Order.ts
@@ -0,0 +1,34 @@
+import { OrderStatus } from './interfaces/OrderStatus';
+import { Messaging } from '../services/Messaging';
+import { Persistency } from '../services/Persistency';
+import { ShoppingCart } from './ShoppingCart';
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open';
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly messaging: Messaging,
+    private readonly persistency: Persistency,
+  ) {}
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu Carrinho está vazio');
+    }
+
+    this._orderStatus = 'closed';
+    this.messaging.sendMessage(
+      `Seu pedido com total de R$${this.cart.total()} foi recebido`,
+    );
+    this.messaging.sendMessage(
+      `Seu pedido total com desconto de R$${this.cart.totalWithDiscount()} foi recebido`,
+    );
+    this.persistency.saveOrder();
+    this.cart.clear();
+  }
+}
