c8cc968966c5463d5fa4d786975617e72d33401d
geisondossantos <geison_bf@hotmail.com>
2022-07-12 18:43:29 -0300

    ISP - Interface Segregation Principle

---
+++ src/isp/classes/ShoppingCart.ts
@@ -0,0 +1,39 @@
+import { Discount } from './Discount';
+import { CartItem } from './interfaces/CartItem';
+
+export class ShoppingCart {
+  private readonly _items: CartItem[] = [];
+
+  constructor(private readonly discount: Discount) {}
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
+
+  totalWithDiscount(): number {
+    return this.discount.calculate(this.total());
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras limpo...');
+    this._items.length = 0;
+  }
+}
